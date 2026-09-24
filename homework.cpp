37df5af329290a4b2300909835e9b2014dd7b6c2
Vo Ho Thai Khang <25ad11001@hcmute.student.edu.vn>
2026-09-24 22:00:33 +0700

    bai 1

---
+++ homework.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+    string storeName;
+    string foodName;
+    cout << "Nhap ten cua hang: ";
+    getline(cin, storeName);
+    cout << "Chao mung den voi " << storeName << "!" << endl;
+
+    
+    return 0;
+}
